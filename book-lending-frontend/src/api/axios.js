fdd20cf4027062ca82dfa28210ac65edec716e82
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2025-08-30 23:22:42 +0530

    new

--- book-lending-frontend/src/api/axios.js
+++ book-lending-frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://booklibrary-iw35.onrender.com/',
 });
 
 API.interceptors.request.use((config) => {
